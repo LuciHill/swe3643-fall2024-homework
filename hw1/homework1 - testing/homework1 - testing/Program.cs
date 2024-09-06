eec9f27b0c06b9bdd8b00ab0d26cc58e4597ea42
LuciHill <lucijmhill@gmail.com>
2024-09-06 11:07:02 -0400

    hw1 set up
    hw1 set up

---
+++ hw1/homework1 - testing/homework1 - testing/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace homework1___testing
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
